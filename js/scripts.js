0c692a3b40f01f0765e2d557149bd1ee51a8dbed
Lucian <lucian.miller185@gmail.com>
2021-06-21 09:44:13 -0700

    It will call function numList with the argument of userNum

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
     const userNum = parseInt($("#number").val());
+    console.log("user num: " + userNum);
+    const finalList = (numList(userNum));
   });
 });
