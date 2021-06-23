d131fc3877a0afbb94b9ba93bc01ff50d686f7b2
Lucian <lucian.miller185@gmail.com>
2021-06-23 10:02:50 -0700

    It will create a for loop to count up from 0 to user number

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+//Business Logic-----------
+function numList(number) {
+  let numArr = []
+  for (let index = 0; index <= number; index++) {
+    numArr.push(index);
+    console.log(numArr);
+  };
+};
+
+numList(10);
+
 //User Logic---------------
-$(document).ready(function() {
-  $("form#input").submit(function(event) {
-    event.preventDefault();
-    const userNum = parseInt($("#number").val());
-    console.log("user num: " + userNum);
-    const finalList = (numList(userNum));
-  });
-});
+// $(document).ready(function() {
+//   $("form#input").submit(function(event) {
+//     event.preventDefault();
+//     const userNum = parseInt($("#number").val());
+//     console.log("user num: " + userNum);
+//     const finalList = (numList(userNum));
+//   });
+// });
