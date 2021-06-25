3b8476271e91f0a0fb1d01e0207a849dd0fe0688
Lucian <lucian.miller185@gmail.com>
2021-06-25 11:12:39 -0700

    It will create a loop to push numbers and correct phrases into output array and display it to the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
 //Business Logic-----------
 function numList(number) {
-  let numArr = []
+  const numArr = []
+  const outputArr = []
   for (let index = 0; index <= number; index++) {
     numArr.push(index);
-    console.log(numArr);
   };
+  numArr.forEach(function(num) {
+    if (num.toString().includes(3)) {
+      outputArr.push("Won't you be my neighbor?");
+    } else if (num.toString().includes(2)) {
+      outputArr.push("Boop!");
+    } else if (num.toString().includes(1)) {
+      outputArr.push("Beep!");
+    } else {
+      outputArr.push(num.toString());
+    };
+  });
+  console.log(outputArr);
+  return outputArr.join(", ");
 };
 
-numList(10);
-
 //User Logic---------------
-// $(document).ready(function() {
-//   $("form#input").submit(function(event) {
-//     event.preventDefault();
-//     const userNum = parseInt($("#number").val());
-//     console.log("user num: " + userNum);
-//     const finalList = (numList(userNum));
-//   });
-// });
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    const userNum = parseInt($("#number").val());
+    console.log("user num: " + userNum);
+    const finalList = (numList(userNum));
+    $("#output").html(finalList);
+  });
+});
